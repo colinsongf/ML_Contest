dfb925bb8522f19c91e99a35560a9032d781ca79
jpsagarm95 <jpsagarm95@gmail.com>
2014-11-04 15:04:56 +0530

    First time Implemented LDA.

---
+++ randomForest.py
@@ -0,0 +1,29 @@
+import numpy as np
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.cross_validation import cross_val_score, StratifiedKFold
+from sklearn.metrics import f1_score
+
+
+data = np.array([ [ float(x) for x in line.split(',') ] for line in open('ML-contest-training/completedData.csv') ])
+print "data done"
+print "logistic initialized"
+# clf.fit(data[:,:-1], data[:,-1])
+print "fitted data"
+skf = StratifiedKFold(data[:,-1], n_folds=5)
+output =[]
+for train, test in skf:
+	clf = RandomForestClassifier(n_estimators = 500)
+	clf = clf.fit([ data[i][:-1] for i in train ], [ data[i][-1] for i in train ])
+	prediction = clf.predict([ data[i][:-1] for i in test ])
+	# pred = []
+	# for i in prediction:
+	# 	if(i > 1.5):
+	# 		pred.append(2)
+	# 	else:
+	# 		pred.append(1)
+	output.append(f1_score([ data[i][-1] for i in test ], prediction))
+	print "done"
+# score = cross_val_score(clf, data[:,:-1], data[:,-1], cv = 5, scoring = 'f1')
+print "out of score"
+for i in output:
+	print i
