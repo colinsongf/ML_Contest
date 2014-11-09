eb74582ff5a073cb74f2f8e64ab7cef2cdbda286
Pratik <spiderpratikp@gmail.com>
2014-11-09 21:00:16 +0530

    k=10 for knn mssing data

---
+++ majorityvote.py
@@ -0,0 +1,19 @@
+import sys
+
+#sys.argv[0] is the .py file
+print sys.argv[1:]
+
+files = [open(f) for f in sys.argv[1:]]
+output = open('majority_vote.txt', 'w')
+
+for i in range(1500):
+	AlessB=0
+	for f in files:
+		if f.readline().strip() == 'A':
+			AlessB += 1
+		else:
+			AlessB -= 1
+	if AlessB >= 0:
+		output.write('A\n')
+	else:
+		output.write('B\n')
